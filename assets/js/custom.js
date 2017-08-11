2aff60690293d1f4f6694345d57d83a2a5d21b61
Thad Roe <git@thadroe.com>
2017-08-11 08:15:05 -0500

    Accordion, reverse home footer on mobile, and more form adjustments

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+(function() {
+
+    var acc = document.getElementsByClassName('accordion');
+
+    if (acc.length) {
+        for (var i = 0; i < acc.length; i++) {
+            acc[i].onclick = function() {
+                this.classList.toggle('active');
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight){
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + 'px';
+                }
+            }
+        }
+    }
+
+    $('#large-form').bind('mousedown keyup', function(e){
+        if ( $(e.target).is('[required]') ) {
+            if( $(e.target).is(':invalid') ){
+                $(e.target).parent().removeClass('valid-icon');
+                $(e.target).parent().addClass('invalid-icon');
+            } else {
+                $(e.target).parent().removeClass('invalid-icon');
+                $(e.target).parent().addClass('valid-icon');
+            }
+        }
+    });
+
+}());
